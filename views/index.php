c590c9f03640414f994dd56c5cecb3cada5dfbb9
jessy Razafindraibe <romeorazafindraibe76@gmail.com>
2025-08-08 13:43:05 +0300

    continuation de projet

--- views/index.php
+++ views/index.php
@@ -1,6 +1,6 @@
 <?php
     include '../models/config.php';
-
+    $name_page = 'Gestion de vente';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,7 +14,7 @@
     <header class="tronc">
         <!-- entete de page -->
         <nav class="menu">
-            <h4 class="page">dashboard</h4>
+            <h4 class="page"><?php htmlspecialchars($name_page); ?></h4>
             <form action="" method="get" class="src_conte">
                 <input type="text" name="query" id="search" placeholder="Rechrercher...">
                 <img src="../public/img/8666693_search_icon.png" alt="" class="search-icon">
@@ -47,7 +47,15 @@
                     <h6 class="menu_name">Dashboard</h6>
                 </a>
             </ul>
-            <div class="profil"></div>
+            <div class="profil">
+                <img src="../public/img/9057332_profile_icon.png" alt="" class="profil-img">
+                <div class="name">
+                    <h6 class="profil-name"><?php htmlspecialchars($_SESSION['']); ?></h6>
+
+                    <p class="first-name"><?php htmlspecialchars($_SESSION['']); ?></p>
+                </div>
+                
+            </div>
         </div>
         <div class="right"></div>
     </header>
